f807fa8bb284ff4afd516e1fc92c60b697ea1040
sir-sandwitch <bodietheburgereater@gmail.com>
2023-09-09 19:05:50 -0600

    start of usb support

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 CFILES = $(wildcard *.c)
 OFILES = $(CFILES:.c=.o)
+LIBFILES = $(wildcard uspi/lib/*.o)
+
 GCCFLAGS = -Wall -O2 -ffreestanding -nostdinc -nostdlib -nostartfiles
 GCCPATH = ~/arm-gnu-toolchain-12.3.rel1-x86_64-aarch64-none-elf/bin
 
@@ -11,9 +13,11 @@ boot.o: boot.S
 %.o: %.c
 	$(GCCPATH)/aarch64-none-elf-gcc $(GCCFLAGS) -c $< -o $@
 
-kernel8.img: boot.o $(OFILES)
-	$(GCCPATH)/aarch64-none-elf-ld -nostdlib boot.o $(OFILES) -T link.ld -o kernel8.elf
+kernel8.img: boot.o $(OFILES) $(LIBFILES)
+	$(GCCPATH)/aarch64-none-elf-ld -nostdlib boot.o $(OFILES) $(LIBFILES) uspi/lib/libuspi.a -T link.ld -o kernel8.elf
 	$(GCCPATH)/aarch64-none-elf-objcopy -O binary kernel8.elf kernel8.img
 
 clean:
 	/bin/rm kernel8.elf *.o *.img > /dev/null 2> /dev/null || true
+
+include uspi/Rules.mk
--- kernel.c
+++ kernel.c
@@ -1,23 +1,74 @@
 #include "io.h"
 #include "fb.h"
+#include "uspi/include/uspios.h"
+#include "uspi/include/uspi.h"
 
-void main()
+// void main()
+// {
+//     uart_init();
+//     fb_init();
+
+//     // drawRect(150,150,400,400,0x03,0);
+//     // drawRect(300,300,350,350,0x2e,1);
+
+//     // drawCircle(960,540,250,0x0e,0);
+//     // drawCircle(960,540,50,0x13,1);
+
+//     // drawPixel(250,250,0x0e);
+
+//     // drawChar('O',500,500,0x05, 3);
+//     // drawString(100,100,"Hello world!",0x0f, 4);
+
+//     // drawLine(100,500,350,700,0x0c);
+
+
+
+//     while (1);
+// }
+
+static const char FromSample[] = "sample";
+
+static void KeyPressedHandler (const char *pString)
 {
-    uart_init();
-    fb_init();
+	ScreenDeviceWrite (USPiEnvGetScreen (), pString, strlen (pString));
+}
 
-    drawRect(150,150,400,400,0x03,0);
-    drawRect(300,300,350,350,0x2e,1);
+int main (void)
+{
+	if (!USPiEnvInitialize ())
+	{
+		return 1;
+	}
+	
+	if (!USPiInitialize ())
+	{
+		LogWrite (FromSample, LOG_ERROR, "Cannot initialize USPi");
 
-    drawCircle(960,540,250,0x0e,0);
-    drawCircle(960,540,50,0x13,1);
+		USPiEnvClose ();
 
-    drawPixel(250,250,0x0e);
+		return 1;
+	}
+	
+	if (!USPiKeyboardAvailable ())
+	{
+		LogWrite (FromSample, LOG_ERROR, "Keyboard not found");
 
-    drawChar('O',500,500,0x05, 3);
-    drawString(100,100,"Hello world!",0x0f, 4);
+		USPiEnvClose ();
 
-    drawLine(100,500,350,700,0x0c);
+		return 1;
+	}
 
-    while (1);
+	USPiKeyboardRegisterKeyPressedHandler (KeyPressedHandler);
+
+	LogWrite (FromSample, LOG_NOTICE, "Just type something!");
+
+	// just wait and turn the rotor
+	for (unsigned nCount = 0; 1; nCount++)
+	{
+		USPiKeyboardUpdateLEDs ();
+
+		ScreenDeviceRotor (USPiEnvGetScreen (), 0, nCount);
+	}
+
+	return 1;
 }
